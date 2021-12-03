7c8f7d4bc8fecde205de2808ff52d7f9ade4fa0c
Gauravsingh Sisodia <xaerru@disroot.org>
2021-12-03 23:57:01 +0530

    feat(dwm): add dwm noborders patch

--- users/xaerru/default.nix
+++ users/xaerru/default.nix
@@ -8,6 +8,10 @@
       dwm = super.dwm.overrideAttrs (oldAttrs: rec {
         patches = [
           ./config/dwm/xaerru-custom-config.diff
+	  (super.fetchpatch {
+            url = "https://dwm.suckless.org/patches/noborder/dwm-noborderfloatingfix-6.2.diff";
+            sha256 = "114xcy1qipq6cyyc051yy27aqqkfrhrv9gjn8fli6gmkr0x6pk52";
+          })
         ];
       });
     })
