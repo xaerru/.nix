14ba40ed30548f1ebcb550aa7423c4f056885aa0
Gauravsingh Sisodia <xaerru@disroot.org>
2021-12-05 00:32:49 +0530

    feat: use home-manager as a module

--- flake.nix
+++ flake.nix
@@ -9,37 +9,43 @@
      };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
 
     pkgs = import nixpkgs {
        inherit system;
        config = { allowUnfree = true; };
+       overlays = [
+         (self: super: {
+      dwm = super.dwm.overrideAttrs (oldAttrs: rec {
+        patches = [
+          ./users/xaerru/config/dwm/xaerru-custom-config.diff
+          (super.fetchpatch {
+            url = "https://dwm.suckless.org/patches/noborder/dwm-noborderfloatingfix-6.2.diff";
+            sha256 = "114xcy1qipq6cyyc051yy27aqqkfrhrv9gjn8fli6gmkr0x6pk52";
+          })
+        ];
+      });})
+       ];
     };
 
     lib = nixpkgs.lib;
   in {
-     homeManagerConfigurations = {
-       xaerru = home-manager.lib.homeManagerConfiguration {
-         inherit system pkgs;
-         username = "xaerru";
-         homeDirectory = "/home/xaerru";
-	 configuration = {
-           imports = [
-             ./users/xaerru
-	   ];
-	 };
-       };
-     };
      nixosConfigurations = {
        nienna = lib.nixosSystem {
          inherit system;
+	 specialArgs = { inherit system inputs pkgs; };
          modules = [
            ./hosts/nienna/configuration.nix
+	   home-manager.nixosModules.home-manager
+          {
+	    home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.xaerru = import ./users/xaerru;
+          }
 	 ];
        };
-
        };
      };
 }
--- users/xaerru/default.nix
+++ users/xaerru/default.nix
@@ -1,19 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  home.username = "xaerru";
+  home.homeDirectory = "/home/xaerru";
   programs.home-manager.enable = true;
   home.packages= with pkgs; [ dwm zoom-us ];
-  nixpkgs.overlays = [
-    (self: super: {
-      dwm = super.dwm.overrideAttrs (oldAttrs: rec {
-        patches = [
-          ./config/dwm/xaerru-custom-config.diff
-	  (super.fetchpatch {
-            url = "https://dwm.suckless.org/patches/noborder/dwm-noborderfloatingfix-6.2.diff";
-            sha256 = "114xcy1qipq6cyyc051yy27aqqkfrhrv9gjn8fli6gmkr0x6pk52";
-          })
-        ];
-      });
-    })
-  ];
 }
