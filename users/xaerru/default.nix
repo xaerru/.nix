57a3c5d77550879d82c1283fb5a239865fa3fc69
Gauravsingh Sisodia <xaerru@disroot.org>
2021-12-06 01:52:46 +0530

    fix(user): put dwm overlay in home.packages

--- users/xaerru/default.nix
+++ users/xaerru/default.nix
@@ -3,6 +3,19 @@
 {
   home.username = "xaerru";
   home.homeDirectory = "/home/xaerru";
+  home.stateVersion = "22.05";
   programs.home-manager.enable = true;
-  home.packages= with pkgs; [ dwm zoom-us ];
+  home.packages = with pkgs; [
+    zoom-us
+    (dwm.overrideAttrs (oldAttrs: rec {
+      patches = [
+        ./config/dwm/xaerru-custom-config.diff
+        (fetchpatch {
+          url =
+            "https://dwm.suckless.org/patches/noborder/dwm-noborderfloatingfix-6.2.diff";
+          sha256 = "114xcy1qipq6cyyc051yy27aqqkfrhrv9gjn8fli6gmkr0x6pk52";
+        })
+      ];
+    }))
+  ];
 }
