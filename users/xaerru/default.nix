786f718a7f71745399499fa3c60bc6d98a00aa1c
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-04 16:07:43 +0530

    feat(xaerru): add gtk and qt themes

--- users/xaerru/default.nix
+++ users/xaerru/default.nix
@@ -1,6 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
-{
+with inputs.nix-colors.lib { inherit pkgs; };
+
+rec {
   imports = [./services/tor.nix];
   home = {
     username = "xaerru";
@@ -139,5 +141,22 @@
 	enableContribAndExtras = true;
 	config = ./config/xmonad/xmonad.hs;
      };
+  };
+  gtk = {
+     enable = true;
+     theme = {
+      name = "default-dark";
+      package = gtkThemeFromScheme { scheme = inputs.nix-colors.colorSchemes.default-dark; };
+    };
+  };
+  qt = {
+     enable = true;
+     platformTheme = "gtk";
+  };
+  services.xsettingsd = {
+    enable = true;
+    settings = {
+      "Net/ThemeName" = "${gtk.theme.name}";
+    };
   };
 }
