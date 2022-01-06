1f28f55b302a9d1ba42f26ca975c5e1b9f69e14d
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-06 15:46:26 +0530

    feat(alacritty): use colors for alacritty theme

--- users/xaerru/default.nix
+++ users/xaerru/default.nix
@@ -2,7 +2,9 @@
 
 with inputs.nix-colors.lib { inherit pkgs; };
 
-rec {
+let 
+colors = inputs.nix-colors.colorSchemes.default-dark.colors;
+in rec {
   imports = [./services/tor.nix];
   home = {
     username = "xaerru";
@@ -27,7 +29,6 @@ rec {
       allow-preset-passphrase
     '';
   };
-  programs.java.enable = true;
   home.packages = with pkgs; [
     keepassxc
     aria
@@ -67,9 +68,59 @@ rec {
     settings = {
       colors = {
         primary = {
-          background = "0x282828";
-          foreground = "0xd8d8d8";
+          background = "#${colors.base00}";
+          foreground = "#${colors.base05}";
         };
+        cursor = {
+          text = "#${colors.base00}";
+          cursor = "#${colors.base05}";
+        };
+        normal = {
+          black = "#${colors.base00}";
+          red = "#${colors.base08}";
+          green = "#${colors.base0B}";
+          yellow = "#${colors.base0A}";
+          blue = "#${colors.base0D}";
+          magenta = "#${colors.base0E}";
+          cyan = "#${colors.base0C}";
+          white = "#${colors.base05}";
+        };
+        bright = {
+          black = "#${colors.base03}";
+          red = "#${colors.base08}";
+          green = "#${colors.base0B}";
+          yellow = "#${colors.base0A}";
+          blue = "#${colors.base0D}";
+          magenta = "#${colors.base0E}";
+          cyan = "#${colors.base0C}";
+          white = "#${colors.base07}";
+        };
+        indexed_colors = [
+          {
+            index = 16;
+            color = "#${colors.base09}";
+          }
+          {
+            index = 17;
+            color = "#${colors.base0F}";
+          }
+          {
+            index = 18;
+            color = "#${colors.base01}";
+          }
+          {
+            index = 19;
+            color = "#${colors.base02}";
+          }
+          {
+            index = 20;
+            color = "#${colors.base04}";
+          }
+          {
+            index = 21;
+            color = "#${colors.base06}";
+          }
+        ];
       };
       env = { TERM = "xterm-256color"; };
       font = { size = 8; };
