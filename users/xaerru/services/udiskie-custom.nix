c2550b0131d9f2cd5c5e2dea276751fe0602dcfb
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-18 16:18:45 +0530

    feat(udiskie): add a service and todo for .xinitrc

---
+++ users/xaerru/services/udiskie-custom.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, lib, ... }:
+
+let
+  cfg = config.services.udiskie-custom;
+  yamlFormat = pkgs.formats.yaml { };
+
+in with lib;
+
+{
+  options.services.udiskie-custom = with types; {
+    enable = mkEnableOption "Enable udiskie daemon";
+    settings = mkOption {
+      type = yamlFormat.type;
+      default = {};
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [pkgs.udiskie];
+    xdg.configFile."udiskie/config.yml" = mkIf (cfg.settings != { }) {
+      source = yamlFormat.generate "udiskie-config" cfg.settings;
+    };
+    # Not to be run as a systemd service
+    # TODO: figure out a way to add these to .xinitrc declaratively
+    #systemd.user.services.udiskie-custom = {
+    #  Unit = {
+    #    Description = "udiskie to automount removable media";
+    #  };
+
+    #  Service = {
+    #    ExecStart = "${pkgs.udiskie}/bin/udiskie"; 
+    # };
+
+    #  Install.WantedBy = [ "default.target" ];
+    #};
+  };
+}
