84c426466274efa5524a594da5437db105a759b3
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-27 09:21:59 +0530

    feat(nix): keep nix-shell outputs and derivations

--- hosts/nienna/configuration.nix
+++ hosts/nienna/configuration.nix
@@ -19,7 +19,9 @@
   nix = {
     package = pkgs.nixUnstable;
     extraOptions = ''
-      experimental-features = nix-command flakes
+        experimental-features = nix-command flakes
+        keep-outputs = true
+        keep-derivations = true
     '';
   };
 
