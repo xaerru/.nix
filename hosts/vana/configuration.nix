034d5561e93b7d07e4e66aa6de691f4edb46ffcc
Gauravsingh Sisodia <xaerru@disroot.org>
2022-02-16 11:45:18 +0530

    feat(vana): add packages

--- hosts/vana/configuration.nix
+++ hosts/vana/configuration.nix
@@ -74,6 +74,10 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     firefox
+    zoom-us
+    git
+    minecraft
+    libreoffice
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
