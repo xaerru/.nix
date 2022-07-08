487286c19b2fa3b5244f064b24d9d6ee3aac2780
Gauravsingh Sisodia <xaerru@disroot.org>
2022-07-08 20:26:43 +0530

    feat(nienna): set cpu to performance

--- hosts/nienna/configuration.nix
+++ hosts/nienna/configuration.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [ ./hardware-configuration.nix ];
+  powerManagement.cpuFreqGovernor = "performance";
 
   nixpkgs.config.packageOverrides = pkgs: {
     vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
