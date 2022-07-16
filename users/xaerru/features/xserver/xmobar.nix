02c3405eb242f53f423a8b933f59a517d81f1c20
Gauravsingh Sisodia <xaerru@disroot.org>
2022-07-16 21:27:05 +0530

    feat(xmobar): switch hardcoded colors to nix-colors

--- users/xaerru/features/xserver/xmobar.nix
+++ users/xaerru/features/xserver/xmobar.nix
@@ -1,5 +1,7 @@
 { pkgs, inputs, config, ... }:
 
+let colors = config.colorscheme.colors;
+in
 {
   programs.xmobar = {
     enable = true;
@@ -9,8 +11,8 @@
                                , "xft:Font Awesome 5 Free Solid:pixelsize=10"
                                , "xft:Font Awesome 5 Brands:pixelsize=10"
                                ]
-           , bgColor = "#2E3440"
-           , fgColor = "#BF616A"
+           , bgColor = "#${colors.base00}"
+           , fgColor = "#${colors.base08}"
            , position = Static { xpos = 0 , ypos = 0, width = 1920, height = 24 }
            , lowerOnStart = True
            , hideOnStart = False
@@ -19,7 +21,7 @@
            , iconRoot = "/home/xaerru/.nix/bin/xpm"
            , commands = [ Run Date "%a %B %e %H:%M" "date" 50
                         , Run Network "wlo1" ["-t", "<fn=2>\xf019</fn> <rx>kb <fn=2>\xf093</fn> <tx>kb"] 20
-                        , Run Cpu ["-t", "<fn=2>\xf108</fn> <total>%","-H","50","--high","#BF616A"] 20
+                        , Run Cpu ["-t", "<fn=2>\xf108</fn> <total>%","-H","50","--high","#${colors.base08}"] 20
                         , Run Memory ["-t","<fn=2>\xf233</fn> <used>M (<usedratio>%)"] 20
                         , Run DiskU [("/", "<fn=2>\xf0c7</fn> <free> free")] [] 60
                         , Run Com "uname" ["-r"] "" 3600
@@ -35,7 +37,7 @@
                               , "--lows"
                               , "Bat: <left>%"
                               , "-O"
-                              , "<fc=#A3BE8C><fn=2>\xf1e6</fn></fc>"
+                              , "<fc=#${colors.base0B}><fn=2>\xf1e6</fn></fc>"
                               , "-o"
                               , ""
                               , "-a"
@@ -49,7 +51,7 @@
                         ]
            , sepChar = "%"
            , alignSep = "}{"
-           , template = " <icon=profile.xpm/> <fc=#666666>|</fc> %UnsafeStdinReader% }{ <fc=#A3BE8C>%music%</fc> <fc=#666666>|</fc> <fc=#8FBCBB>%uname%</fc> <fc=#666666>|</fc> <fc=#A3BE8C>%disku%</fc> <fc=#666666>|</fc> <fc=#B48EAD>Vol: %sound%</fc> <fc=#666666>|</fc> <fc=#88C0D0>%memory%</fc> <fc=#666666>|</fc> <fc=#B48EAD>%cpu%</fc> <fc=#666666>|</fc> <fc=#88C0D0>%wlo1%</fc> <fc=#666666>|</fc> <fc=#A3BE8C>%battery%</fc> <fc=#666666>|</fc> <fc=#D8DEE9>%date%</fc> %trayerpad%"
+           , template = " <icon=profile.xpm/> <fc=#${colors.base04}>|</fc> %UnsafeStdinReader% }{ <fc=#${colors.base0B}>%music%</fc> <fc=#${colors.base04}>|</fc> <fc=#${colors.base0C}>%uname%</fc> <fc=#${colors.base04}>|</fc> <fc=#${colors.base0B}>%disku%</fc> <fc=#${colors.base04}>|</fc> <fc=#${colors.base0E}>Vol: %sound%</fc> <fc=#${colors.base04}>|</fc> <fc=#${colors.base0D}>%memory%</fc> <fc=#${colors.base04}>|</fc> <fc=#${colors.base0E}>%cpu%</fc> <fc=#${colors.base04}>|</fc> <fc=#${colors.base0D}>%wlo1%</fc> <fc=#${colors.base04}>|</fc> <fc=#${colors.base0B}>%battery%</fc> <fc=#${colors.base04}>|</fc> <fc=#${colors.base07}>%date%</fc> %trayerpad%"
     '';
   };
 }
