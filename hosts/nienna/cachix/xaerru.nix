bf474f3bbe1a038db5ed85286016c4c9b75879a9
Gauravsingh Sisodia <xaerru@gmail.com>
2024-02-25 12:16:22 +0530

    fix: cachix

---
+++ hosts/nienna/cachix/xaerru.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://xaerru.cachix.org"
+      ];
+      trusted-public-keys = [
+        "xaerru.cachix.org-1:zcy0gdAeQe9lXcrNGQ4FAo3PE5fhQisD+MxW7V7nuaE="
+      ];
+    };
+  };
+}
